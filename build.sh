fbcae42773c6bff5fc8d7633455a703fb0753320
Dafin Surya <dafinsurya111@gmail.com>
2026-06-23 08:40:49 +0700

    fix: add Node.js runtime

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# build.sh
-# Unduh ffmpeg static build untuk Linux
-curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
-tar -xf ffmpeg.tar.xz
-mv ffmpeg-*-static/ffmpeg ./ffmpeg
-chmod +x ./ffmpeg
